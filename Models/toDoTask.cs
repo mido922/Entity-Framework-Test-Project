7f10309ecc608dc466446beae603d6f8a26ccc73
mido922 <mzohry@gmail.com>
2024-09-29 14:25:46 +0300

    Add project files.

---
+++ Models/toDoTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Test_Project.Models
+{
+    public class toDoTask
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using (var context = new MyDbContext())
+{
+    var basicToDoTask = new toDoTask()
+    {
+        Name = "Clean House",
+        Status = "Unfinished.",
+    };
+
+    context.toDoTasks.Add(basicToDoTask);
+
+    context.SaveChanges();
+
+    var allToDoTasks = context.toDoTasks.ToList();
+} 
+
+
+
+public class MyDbContext : DbContext
+{
+    public DbSet<toDoTask> toDoTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TestTaskDB");
+    }
+
+
+}
+
+public class toDoTask
+{
+    public string Name { get; set; }
+    public string Status { get; set; }
+
+
+}
